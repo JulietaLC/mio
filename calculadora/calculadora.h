a96279151e631bb21d44979678d88b6ae7b77678
Leiva <leivaju@tasa.telefonica.com.ar>
2019-04-04 22:59:10 -0300

    calc

---
+++ calculadora/calculadora.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+float sumar(float A,float B);
+float restar(float A,float B);
+float multiplicar(float A,float B);
+float dividir (float A, float B);
+float factorial (float A);
+
